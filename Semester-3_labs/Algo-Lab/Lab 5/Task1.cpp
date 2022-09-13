148fd67eec9f452d242d04bd77b66628d925884f
Ronit Chinda <ronitchinda100@gmail.com>
2022-09-13 15:57:42 +0530

    Updated Lab 5 Task1

--- Semester-3_labs/Algo-Lab/Lab 5/Task1.cpp
+++ Semester-3_labs/Algo-Lab/Lab 5/Task1.cpp
@@ -3,40 +3,41 @@ using namespace std;
 
 int main()
 {
-    int n,capacity;
-    cout<<"Please enter total number of Denomination"<<endl;
+    int n, capacity;
+    cout << "Please enter total number of Denomination" << endl;
     cin >> n;
     vector<int> v(n);
     vector<int> v1(n);
-    cout<<"Please enter denominations : "; 
+    cout << "Please enter denominations : ";
     for (int i = 0; i < n; i++)
     {
         int x;
-        cin>>x;
-        v[i]=x;
+        cin >> x;
+        v[i] = x;
     }
 
     sort(v.begin(), v.end(), greater<int>());
-    cout<<"Please enter the amount you want to change : "<<endl;
-    cin>>capacity;
+    cout << "Please enter the amount you want to change : " << endl;
+    cin >> capacity;
 
     for (int i = 0; i < n; ++i)
     {
-        if(capacity>=v[i]){
-
-             v1[i]=capacity/v[i];
-             capacity -= (capacity*v1[i]);
-        }     
-    }
-    cout<<"Your Denomination :"<<endl;
-    cout<<"-------------------------------"<<endl;
-    for (int i = 0; i < n; ++i)
-    {
-        if (v1[i]>0)
+        if (capacity >= v[i])
         {
-            cout<<v1[i]<<" * "<<v[i]<<" Rs coin"<<endl;
+            v1[i] = capacity / v[i];
+            capacity -= v1[i] * v[i];
         }
     }
-    cout<<"-------------------------------"<<endl;
-    
+
+    cout << "Your Denomination :" << endl;
+    cout << "-------------------------------" << endl;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (v1[i] > 0)
+        {
+            cout << v1[i] << " * " << v[i] << " Rs coin" << endl;
+        }
+    }
+    cout << "-------------------------------" << endl;
 }
